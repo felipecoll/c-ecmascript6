1738a20d0b11512370a7024c6d6efa4437fa501b
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-02 16:36:09 -0300

    strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,16 @@
+let hello = 'Hello'
+let world = 'world'
+
+let epicPhrase = hello + ' ' + world + '!'
+console.log(epicPhrase) 
+
+// Template literals
+
+let epicPhrase2 = `${hello} ${world}!`
+console.log(epicPhrase2)
+
+
+// Multi lineas
+
+let lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \n' 
++ 'Pellentesque nec turpis nec nunc tincidunt fermentum. \n'
