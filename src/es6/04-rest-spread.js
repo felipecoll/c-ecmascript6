7a2590b91f62d8929d965fe6cb124236d91dc88d
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-02 16:55:42 -0300

    rest-spread

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,15 @@
+// Arrays desestructurados
+
+let fruits = ['apple', 'banana', 'orange', 'kiwi'];
+
+let [a, b] = fruits;
+
+console.log(a, b, fruits[3])
+
+// Objeto desestructurado
+
+let user = {username: 'Pedrito', age: 34}
+
+let {username, age} = user;
+
+console.log(username, age)
