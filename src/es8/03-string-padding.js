8fe54bdc6974624e6e1b123346039196276d1fef
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-04 07:37:21 -0300

    ES8 Fin

---
+++ src/es8/03-string-padding.js
@@ -0,0 +1,4 @@
+const string = 'Hello'
+
+console.log(string.padStart(5, 'Hi ')) //
+console.log(string.padEnd(6, '_')) 
---
+++ src/es8/04-trailling-commas.js
@@ -0,0 +1,4 @@
+const ages = [23,56,34,21,57, , , , ]
+
+console.log(array)
+console.log(array.length)
