1f0714c2a02a9975669b01c0030a9168871a7916
Felipe Coll <leonfelipecoll@gmail.com>
2025-02-04 20:18:46 -0300

    E11 - nullish and biginit

---
+++ src/es11/00-optional-chaininig.js
@@ -0,0 +1,10 @@
+const users = {
+    philip : {
+        country: 'Ar'
+    },
+    john: {
+        country: 'Br'
+    }
+}
+
+console.log(users?.bebeloper?.country)
---
+++ src/es11/01-biginit.js
@@ -0,0 +1,4 @@
+const aBigNumber = 73895689346502935623n
+
+const anotherBigNumber = BigInt(73895689346502935623)
+
